a000fe117a35c685a5a0fd7260801dc422af09a9
HLC <hansly.agbamate@epitech.eu>
2025-02-04 15:34:26 +0100

    add book service implementation

--- borrowing-service/src/main/java/com/example/borrowing_service/BorrowingServiceApplication.java
+++ borrowing-service/src/main/java/com/example/borrowing_service/BorrowingServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.borrowing_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BorrowingServiceApplication {
 
 	public static void main(String[] args) {
